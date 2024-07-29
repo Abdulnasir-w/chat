37c4e20f16f22e05de4111bed15a9f3d9d4e0b06
Abdul Nasir <abdulnasir9323@gmail.com>
2024-07-29 15:12:28 -0700

    add validation

--- lib/Components/custom_text_form.dart
+++ lib/Components/custom_text_form.dart
@@ -6,24 +6,31 @@ class MyCustomTextFormField extends StatefulWidget {
   final IconData prefixIcon;
   final TextEditingController? controller;
   final TextInputType keyBoardType;
+  final String? Function(String?)? validator;
+  final bool isPassField;
 
-  const MyCustomTextFormField(
-      {super.key,
-      required this.label,
-      required this.prefixIcon,
-      this.controller,
-      required this.hint,
-      required this.keyBoardType});
+  const MyCustomTextFormField({
+    super.key,
+    required this.label,
+    required this.hint,
+    required this.keyBoardType,
+    this.validator,
+    this.controller,
+    required this.prefixIcon,
+    this.isPassField = false,
+  });
 
   @override
   State<MyCustomTextFormField> createState() => _MyCustomTextFormFieldState();
 }
 
 class _MyCustomTextFormFieldState extends State<MyCustomTextFormField> {
+  bool isVisible = true;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: widget.controller,
+      obscureText: widget.isPassField ? isVisible : false,
       keyboardType: widget.keyBoardType,
       decoration: InputDecoration(
         label: Text(
@@ -33,7 +40,18 @@ class _MyCustomTextFormFieldState extends State<MyCustomTextFormField> {
         hintText: widget.hint,
         hintStyle: const TextStyle(color: Color(0xFF4A90E2)),
         prefixIcon: Icon(widget.prefixIcon),
-        suffixIcon: const Icon(Icons.visibility_off_outlined),
+        suffixIcon: widget.isPassField
+            ? IconButton(
+                onPressed: () {
+                  isVisible = !isVisible;
+                },
+                icon: Icon(
+                  isVisible
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                ),
+              )
+            : null,
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
         ),
@@ -50,6 +68,7 @@ class _MyCustomTextFormFieldState extends State<MyCustomTextFormField> {
           borderSide: const BorderSide(color: Colors.red, width: 2),
         ),
       ),
+      validator: widget.validator,
     );
   }
 }
---
+++ lib/Utils/validation.dart
@@ -0,0 +1,21 @@
+class Validator {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Email Field Cannot be Empty";
+    }
+    final RegExp emailRegex = RegExp(
+      r'^[^@]+@[^@]+\.[^@]+',
+    );
+    if (emailRegex.hasMatch(value)) {
+      return "Enter a Valid Email";
+    }
+    return null;
+  }
+
+  static String? validateField(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Field cannot be Empty";
+    }
+    return null;
+  }
+}
